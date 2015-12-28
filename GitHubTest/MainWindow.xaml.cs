6144b2518215472efe296caa241a94e4d57334ca
YoungSunPark <developer@helloworlds.co.kr>
2015-12-28 12:55:51 +0900

    Commit!!

--- GitHubTest/MainWindow.xaml.cs
+++ GitHubTest/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace GitHubTest
         {
             InitializeComponent();
             Console.WriteLine("First commit");
-            Console.WriteLine("Second commit");
+            Console.WriteLine("second commit");
         }
     }
 }
